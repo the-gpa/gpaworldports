fb2629e5cd503a157683356125b5001fa7c439f3
keikreutler <kei@ourmachine.net>
2016-08-22 17:18:17 +0200

    Updated repo to live production site theme

--- footer.php
+++ footer.php
@@ -1,35 +1,24 @@
 <?php
 /**
- * The template for displaying the footer.
- *
- * Contains the closing of the class=site-inner div and all content after
- *
- */
+* The template for displaying the footer.
+*
+* Contains the closing of the class=site-inner div and all content after
+*
+*/
 ?>
 
-	</div><!-- .site-inner -->
+    </div><!-- .site-inner -->
 </div>
 
 <footer id="footer" class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter"><div class="container"><div class="row">
-	<div class="footer-content footer-insert">
-
-		<p class="copyright">Privacy and copyright
- 
-		Name and logo Sitemap
-		 
-		Jobs
-		 
-		FAQs
-		 
-		Contact ISO</p>
-
-		<p>We are committed to ensuring that our website is accessible to everyone. If you have any questions or suggestions regarding the accessibility of this site, please contact us.</p>
-
-		<p>© Some Rights Reserved.</p>
-	</div>
+    <div class="footer-content footer-insert">
+        <p>We are committed to ensuring that our website is accessible to everyone. If you have any questions or suggestions regarding the accessibility of this site, please <a href="mailto:hq@gpaworldports.org">contact us.</a></p>
+<br><br>
+        <p>© Some Rights Reserved.</p>
+    </div>
 </footer>
 
-	<?php wp_footer(); ?>
+    <?php wp_footer(); ?>
 
 </body>
 </html>
